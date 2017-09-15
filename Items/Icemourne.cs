69ac04e3875364d650e22b0cf1c0ab842eb547ef
akinde <akind@oregonstate.edu>
2017-09-14 19:36:45 -0700

    Uploading Initial commit.

---
+++ Items/Icemourne.cs
@@ -0,0 +1,40 @@
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Console_Port_Mod.Items
+{
+	public class Icemourne : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Icemourne");
+			Tooltip.SetDefault("");
+		}
+		public override void SetDefaults()
+		{
+			item.damage = 44;
+			item.melee = true;
+			item.width = 78;
+			item.height = 78;
+			item.useTime = 20;
+			item.useAnimation = 20;
+			item.useStyle = 1;
+			item.knockBack = 8;
+			item.value = 120;
+			item.rare = 6;
+			item.UseSound = SoundID.Item1;
+			item.autoReuse = true;
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.IceBlade, 1);
+            recipe.AddIngredient(ItemID.IceFeather, 10);
+            recipe.AddIngredient(ItemID.IceSickle, 1);
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
---
+++ Items/Sharanga.cs
@@ -0,0 +1,41 @@
+﻿using Terraria;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+using Terraria.ModLoader;
+
+namespace Mods.Items
+{
+    public class Windbow : ModItem
+    {
+        public override void SetDefaults()
+        {
+            item.damage = 35;
+            item.noMelee = true;
+            item.ranged = true;
+            item.width = 69;
+            item.height = 40;
+            item.useTime = 30;
+            item.useAnimation = 30;
+            item.useStyle = 5;
+            item.shoot = 3;
+            item.useAmmo = 1;
+            item.knockBack = 1;
+            item.value = 120;
+            item.rare = 7;
+            item.autoReuse = true;
+            item.shootSpeed = 10f;
+        }
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+            Projectile.NewProjectile(position.X, position.Y, speedX, speedY, ProjectileID.FrostburnArrow, damage, knockBack, player.whoAmI, 0f, 0f);
+            return false;
+        }
+        /*public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.DirtBlock, 1);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }*/
+    }
+}
